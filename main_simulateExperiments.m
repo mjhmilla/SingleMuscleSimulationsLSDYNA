6acb194606d34d3269aee24246e743c740cd9e36
Matthew Millard <millard.matthew@gmail.com>
2022-11-24 16:17:38 +0100

    sinusoid simulation now begins with an initial velocity

--- main_simulateExperiments.m
+++ main_simulateExperiments.m
@@ -26,13 +26,13 @@ flag_preProcessSimulationData       = 0;
 %experiments. At the moment this is limited to generating the random perturbation
 %signals used in the impedance experiments.
 
-flag_runSimulations                 = 0;
+flag_runSimulations                 = 1;
 %Setting this to 1 will run the simulations that have been enabled
 
-flag_postProcessSimulationData      = 1;
+flag_postProcessSimulationData      = 0;
 %Setting this to 1 will generate plots of the enabled experiments
 
-flag_generateGenericPlots           = 0;
+flag_generateGenericPlots           = 1;
 flag_generateSpecificPlots          = 1;
 
 
@@ -41,8 +41,8 @@ flag_enableConcentricExperiment         = 0;
 flag_enableQuickReleaseExperiment       = 0;
 flag_enableEccentricExperiment          = 0;
 flag_enableImpedanceExperiment          = 0;
-flag_enableSinusoidExperiment           = 0;
-flag_enableReflexExperiment             = 1;
+flag_enableSinusoidExperiment           = 1;
+flag_enableReflexExperiment             = 0;
 flag_enableReflexExperiment_kN_mm_ms    = 0;
 
 flag_aniType = 1; 
