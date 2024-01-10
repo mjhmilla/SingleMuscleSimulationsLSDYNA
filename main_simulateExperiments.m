e8fda6185381322d872491ba4cb0d0d385aff1e4
Matthew Millard <millard.matthew@gmail.com>
2024-01-10 16:36:19 +0100

    both max and submax simulations of umat41/active_passive_force_length are running using Hatzes activation dynamics

--- main_simulateExperiments.m
+++ main_simulateExperiments.m
@@ -55,7 +55,7 @@ flag_preProcessSimulationData       = 0;
 %experiments. At the moment this is limited to generating the random perturbation
 %signals used in the impedance experiments.
 
-flag_runSimulations                 = 0;
+flag_runSimulations                 = 1;
 %Setting this to 1 will run the simulations that have been enabled
 
 flag_postProcessSimulationData      = 1;
