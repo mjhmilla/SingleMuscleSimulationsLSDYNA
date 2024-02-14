c59244a6fa29cf992b3038e97a792009ffaf1e17
mjhmilla <millard.matthew@gmail.com>
2024-02-14 17:07:59 +0100

    mat156 active and passive force-length curves fit for HL1997

--- rsync_itm_publication_results.sh
+++ rsync_itm_publication_results.sh
@@ -7,7 +7,10 @@ sim_dir_local=$PWD/"MPP_R931"
 
 rsync -r ${ssh_itm}:${sim_dir_itm}/mat156/active_passive_force_length/ ${sim_dir_local}/mat156/active_passive_force_length/
 rsync -r ${ssh_itm}:${sim_dir_itm}/mat156/force_velocity/ ${sim_dir_local}/mat156/force_velocity/
-rsync -r ${ssh_itm}:${sim_dir_itm}/umat41/active_passive_force_length/ ${sim_dir_local}/umat41/active_passive_force_length/
-rsync -r ${ssh_itm}:${sim_dir_itm}/umat41/force_velocity/ ${sim_dir_local}/umat41/force_velocity/
-rsync -r ${ssh_itm}:${sim_dir_itm}/umat43/active_passive_force_length/ ${sim_dir_local}/umat43/active_passive_force_length/
-rsync -r ${ssh_itm}:${sim_dir_itm}/umat43/force_velocity/ ${sim_dir_local}/umat43/force_velocity/
+
+#rsync -r ${ssh_itm}:${sim_dir_itm}/umat41/active_passive_force_length/ ${sim_dir_local}/umat41/active_passive_force_length/
+#rsync -r ${ssh_itm}:${sim_dir_itm}/umat41/force_velocity/ ${sim_dir_local}/umat41/force_velocity/
+
+#rsync -r ${ssh_itm}:${sim_dir_itm}/umat43/active_passive_force_length/ ${sim_dir_local}/umat43/active_passive_force_length/
+#rsync -r ${ssh_itm}:${sim_dir_itm}/umat43/force_velocity/ ${sim_dir_local}/umat43/force_velocity/
+
