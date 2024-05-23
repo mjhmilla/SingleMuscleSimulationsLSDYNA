5d4f15249ed0152f1c1319a8454d0d7a680c574d
Matthew Millard <millard.matthew@gmail.com>
2024-05-23 13:28:45 +0200

    update to main_fitAndRun in progress

--- main_fitAndRun.m
+++ main_fitAndRun.m
@@ -2,27 +2,51 @@ clc;
 close all;
 clear all;
 
+flag_fit=0;
+
+flag_runFal		=	1
+flag_runHL1997	=	1;
+flag_runHL2002	=	0;
+flag_runKBR1994=	1;
 %Fit curves to data
-expData='HL2002';
-main_fitMuscles;
-expData='HL1997';
-main_fitMuscles;
 
+if(flag_fit==1)
+	expData='HL2002';
+	main_fitMuscles;
+	expData='HL1997';
+	main_fitMuscles;
+end
 %Numerically polish the starting lengths, and titin properties
+
+if(flag_fit==1)
+	clc;
+	close all;
+	clearvars -except flag_fit, flag_runFal, flag_runHL1997, flag
+	main_fitMusclesToHL2002Simulation;
+end
+
+
 clc;
 close all;
 clear all;
-main_fitMusclesToHL2002Simulation;
 
-%Run the simulations
-clc;
-close all;
-clear all;
-
-simulationConfig.type='HL2002';
+simulationConfig.type='fal';
 simulationConfig.mode='run';
 main_simulateExperiments;
 
+
+
+
+%Run the simulations
+if(flag_runHL2002==1)
+	clc;
+	close all;
+	clear all;
+
+	simulationConfig.type='HL2002';
+	simulationConfig.mode='run';
+	main_simulateExperiments;
+end
 % clc;
 % close all;
 % clear all;
