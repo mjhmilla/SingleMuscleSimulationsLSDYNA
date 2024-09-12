91874e8d08ed428de796371b9ef122a4f2f98aad
Matthew Millard <millard.matthew@gmail.com>
2024-09-12 17:55:32 +0200

    reverted flag settings of main_simulateExperiments back to a state where it can be run as part of the larger outer loop

--- main_simulateExperiments.m
+++ main_simulateExperiments.m
@@ -4,7 +4,7 @@
 % SPDX-License-Identifier: MIT
 %
 %%
-flag_outerLoopMode=0;
+flag_outerLoopMode=1;
 
 if(flag_outerLoopMode==0)
     clc;
@@ -145,7 +145,7 @@ if(flag_enableForceVelocityExperimentViva ...
     disp('         a publication.');
 end
 
-runOneTrial = ['ramp_400mmps_4mm'];
+runOneTrial = [];
 
 flag_sinusoid_aniType = 0; 
 % This is only relevant when post-processing SinusoidExperiment
